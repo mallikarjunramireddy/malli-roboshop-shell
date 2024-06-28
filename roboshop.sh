4dfcb166fb89d4af97438e009653d4da38964409
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2024-06-28 19:00:21 +0200

    made changes on roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
+SG_ID=sg-06b9e0dd4fd1571c5 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="daws76s.online"
+ZONE_ID=Z08913253SP498RYPTDOA # replace your zone ID
+DOMAIN_NAME="medirectdevops.online"
 
 for i in "${INSTANCES[@]}"
 do
